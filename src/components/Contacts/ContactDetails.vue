e30c4f287d700f24020bfbe3ead00da217727e32
SK Dutta <skd@officeclip.com>
2023-08-29 17:23:08 -0400

    implemented visible permission on the view screen

--- src/components/Contacts/ContactDetails.vue
+++ src/components/Contacts/ContactDetails.vue
@@ -23,58 +23,60 @@ onMounted(() => {
 
 <template>
   <q-card
-        v-for="(section, index) in contactDetails?.sections"
-        :key="index"
-        flat
-        bordered
-        class="relative-position card-example"
-      >
-        <q-card-section class="q-pb-none">
-          <div class="text-h6">{{ section.sectionName }}</div>
-        </q-card-section>
-        <q-card-section class="q-pb-none">
-          <q-list>
-            <q-item>
-              <q-item-section class="rowItems align-content-left">
-                <!-- From: https://stackoverflow.com/a/62785796/89256 -->
-                <div
-                  v-for="(sectionEntry, i) in section.sectionEntries"
-                  :key="i"
-                >
-                  <div v-if="!(i % 2)">
-                    <q-item-label caption>{{
-                      sectionEntry.visibleName
-                    }}</q-item-label>
-                    <q-item-label class="q-mb-md">{{
-                      sectionEntry.value
-                    }}</q-item-label>
-                  </div>
-                </div>
-              </q-item-section>
-              <q-item-section class="rowItems align-content-right">
-                <div
-                  v-for="(sectionEntry, i) in section.sectionEntries"
-                  :key="i"
-                >
-                  <div v-if="i % 2">
-                    <q-item-label caption>{{
-                      sectionEntry.visibleName
-                    }}</q-item-label>
-                    <q-item-label class="q-mb-md">{{
-                      sectionEntry.value
-                    }}</q-item-label>
-                  </div>
-                </div>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-card-section>
-      </q-card>
-      <q-card>
-        <div>
-          <!-- <pre>{{ contactDetails }}</pre> -->
-        </div>
-      </q-card>
+    v-for="(section, index) in contactDetails?.sections"
+    :key="index"
+    flat
+    bordered
+    class="relative-position card-example"
+  >
+    <q-card-section class="q-pb-none">
+      <div class="text-h6">{{ section.sectionName }}</div>
+    </q-card-section>
+    <q-card-section class="q-pb-none">
+      <q-list>
+        <q-item>
+          <q-item-section class="rowItems align-content-left">
+            <!-- From: https://stackoverflow.com/a/62785796/89256 -->
+            <div v-for="(sectionEntry, i) in section.sectionEntries" :key="i">
+              <div v-if="!(i % 2)">
+                <q-item-label caption>{{
+                  sectionEntry.visibleName
+                }}</q-item-label>
+                <q-item-label v-if="sectionEntry.visible" class="q-mb-md">{{
+                  sectionEntry.value
+                }}</q-item-label>
+                <q-item-label v-else caption class="q-mb-md text-italic">
+                  <q-icon name="hide_source" /> You do not permission to view
+                  this item
+                </q-item-label>
+              </div>
+            </div>
+          </q-item-section>
+          <q-item-section class="rowItems align-content-right">
+            <div v-for="(sectionEntry, i) in section.sectionEntries" :key="i">
+              <div v-if="i % 2">
+                <q-item-label caption>{{
+                  sectionEntry.visibleName
+                }}</q-item-label>
+                <q-item-label v-if="sectionEntry.visible" class="q-mb-md">{{
+                  sectionEntry.value
+                }}</q-item-label>
+                <q-item-label v-else caption class="q-mb-md text-italic">
+                  <q-icon name="hide_source" /> You do not permission to view
+                  this item
+                </q-item-label>
+              </div>
+            </div>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-card-section>
+  </q-card>
+  <q-card>
+    <div>
+      <!-- <pre>{{ contactDetails }}</pre> -->
+    </div>
+  </q-card>
 </template>
 <style scoped>
 .rowItems {
