c9aad54c65baac9e8b330c53c8546470def67bd5
Kathiravan Sekar <kathiravanrs@gmail.com>
2023-09-05 12:09:35 -0400

    Replaced all direct network calls to get user photo with a store (Fixed Typos)

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -41,8 +41,8 @@ const session: ComputedRef<Session> = computed(() => {
 })
 
 const userProfile: ComputedRef<UserProfile> = computed(()=>{
-  console.log('UserProfile store', userProfileStore.userProfiles[0]);
-  return userProfileStore.userProfiles[0];
+  console.log('UserProfile store', userProfileStore.UserProfiles[0]);
+  return userProfileStore.UserProfiles[0];
 })
 
 const organizationItems = computed(() => {
