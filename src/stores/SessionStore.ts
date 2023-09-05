c37af257c038bcc2eb7864ce9bfcd8d14fdbb486
Sudhakar <sudhakar@officeclip.com>
2023-09-05 12:37:19 +0530

    minor syntax fix

--- src/stores/SessionStore.ts
+++ src/stores/SessionStore.ts
@@ -1,6 +1,6 @@
-import {defineStore} from 'pinia';
+import { defineStore } from 'pinia';
 import axios from 'axios';
-import {Session} from 'src/models/session';
+import { Session } from 'src/models/session';
 
 export const useSessionStore = defineStore('sessionStore', {
   state: () => ({
@@ -16,7 +16,7 @@ export const useSessionStore = defineStore('sessionStore', {
       try {
         const data = await axios.get('http://localhost:4000/session');
         this.sessions = data.data;
-        console.log("Test: " , data.data);
+        console.log('Test: ', data.data);
       } catch (error) {
         alert(error);
         console.log(error);
