f76f4f67937eb8da16a6b65a1472ac32e673524f
SK Dutta <skd@officeclip.com>
2023-09-21 15:09:24 -0400

    WIP

--- src/components/Expenses/details/autoRentalExpense.vue
+++ src/components/Expenses/details/autoRentalExpense.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { defineProps, ref, Ref } from 'vue';
 import { autoRentalExpense } from '../../../models/expense/expenseDetails';
+import dateTimeHelper from '../../../helpers/dateTimeHelper';
 
 const props = defineProps(['expense']);
 const autoRentalExpense: Ref<autoRentalExpense | undefined> = ref();
@@ -11,10 +12,20 @@ console.log(`autoRentalExpense: ${props.expense}`);
 </script>
 
 <template>
-  <q-item-section class="q-ma-md">
-    <q-item-label caption> From Date </q-item-label>
-    <q-item-label class="q-mb-sm">
-      {{ autoRentalExpense?.fromDate }}
-    </q-item-label>
-  </q-item-section>
+  <q-item-label caption> From Date </q-item-label>
+  <q-item-label class="q-mb-sm">
+    {{ dateTimeHelper.extractDateFromUtc(autoRentalExpense?.fromDate) }}
+  </q-item-label>
+  <q-item-label caption> To Date </q-item-label>
+  <q-item-label class="q-mb-sm">
+    {{ dateTimeHelper.extractDateFromUtc(autoRentalExpense?.toDate) }}
+  </q-item-label>
+  <q-item-label caption> Rental Agency </q-item-label>
+  <q-item-label class="q-mb-sm">
+    {{ autoRentalExpense?.rentalAgency }}
+  </q-item-label>
+  <q-item-label caption> City </q-item-label>
+  <q-item-label class="q-mb-sm">
+    {{ autoRentalExpense?.city }}
+  </q-item-label>
 </template>
