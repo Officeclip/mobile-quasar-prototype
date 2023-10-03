2b78b7fde729c4e27db911f9c8e65d8183ca6835
Kathiravan Sekar <kathiravanrs@gmail.com>
2023-10-03 17:56:00 -0400

    Implemented Search task

--- src/pages/Tasks/TasksList.vue
+++ src/pages/Tasks/TasksList.vue
@@ -1,16 +1,47 @@
 <script lang="ts" setup>
-import {ref} from 'vue';
-import TasksList from 'components/tasks/tasksListCtrl.vue';
+import {computed, onBeforeMount, ref} from 'vue';
+import TaskSummaryItem from "components/tasks/TaskSummaryItem.vue";
+import {useTaskSummaryStore} from "stores/task/taskSummaryStore";
+import {taskSummary} from "src/models/task/taskSummary";
 
-const model = ref(null);
-const ownedByMe = ref(false);
-const assignedToMe = ref(false);
+const filterString = ref('');
+const ownedByMeFilter = ref(false);
+const assignedToMeFilter = ref(false);
 const userName = ref("Alice Johnson");
 const parent = ref({
   parentObjectId: -1,
   parentObjectServiceType: -1,
 });
-const options = ['Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'];
+
+const taskSummaryStore = useTaskSummaryStore();
+
+const getTasks = computed(() => {
+  let filteredTasks = taskSummaryStore.taskSummaries;
+
+  if (filterString.value) {
+    filteredTasks = filteredTasks.filter((task: taskSummary) => {
+      return task.subject.toLowerCase().includes(filterString.value.toLowerCase());
+    });
+  }
+
+  if (ownedByMeFilter.value === true) {
+    filteredTasks = filteredTasks.filter((task: taskSummary) => {
+      return task.taskOwner === userName.value;
+    });
+  }
+
+  if (assignedToMeFilter.value === true) {
+    filteredTasks = filteredTasks.filter((task: taskSummary) => {
+      return task.assignee.includes(userName.value);
+    });
+  }
+  return filteredTasks;
+});
+
+onBeforeMount(() => {
+  taskSummaryStore.getTasks(Number(parent.value.parentObjectId), Number(parent.value.parentObjectServiceType));
+});
+
 </script>
 
 <template>
@@ -18,12 +49,12 @@ const options = ['Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'];
     <q-header bordered class="bg-primary text-white" height-hint="98" reveal>
       <q-toolbar>
         <q-btn
-          color="white"
-          dense
-          flat
-          icon="arrow_back"
-          round
-          @click="$router.go(-1)"
+            color="white"
+            dense
+            flat
+            icon="arrow_back"
+            round
+            @click="$router.go(-1)"
         >
         </q-btn>
         <q-toolbar-title> Tasks</q-toolbar-title>
@@ -34,35 +65,36 @@ const options = ['Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'];
       <q-page>
         <div class="q-pa-sm row text-center">
           <div class="col-4">
-            <q-select
-              v-model="model"
-              :options="options"
-              dense
-              label="Select Filter"
-              outlined
+            <q-input
+                v-model="filterString"
+                label="Search"
+                outlined
             />
           </div>
           <div class="col-3">
-            <q-checkbox v-model="ownedByMe" dense label="Owned by me"/>
+            <q-checkbox v-model="ownedByMeFilter" dense label="Owned by me"/>
           </div>
           <div class="col-3">
-            <q-checkbox v-model="assignedToMe" dense label="Assigned to me"/>
+            <q-checkbox v-model="assignedToMeFilter" dense label="Assigned to me"/>
           </div>
         </div>
-        <TasksList :params="parent" :user="userName" :ownerFilter="ownedByMe" :assigneeFilter="assignedToMe"/>
+
+        <q-list v-for="task in getTasks" :key="task.id" class="q-pa-xs">
+          <taskSummaryItem :task="task"/>
+        </q-list>
       </q-page>
       <q-page-sticky :offset="[18, 18]" position="bottom-right">
         <q-btn
-          :to="{
+            :to="{
             name: 'newTask',
             params: {
               id: -1,
             },
           }"
-          color="accent"
-          fab
-          icon="add"
-          padding="sm"
+            color="accent"
+            fab
+            icon="add"
+            padding="sm"
         />
       </q-page-sticky>
     </q-page-container>
