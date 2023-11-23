d7dcfaabb5b474363ca7b91d950cfc575206a7b7
SK Dutta <skd@officeclip.com>
2023-11-23 17:49:56 -0500

    added validation to the login form

--- src/pages/dTest.vue
+++ src/pages/dTest.vue
@@ -3,6 +3,9 @@ import { Ref, ref } from 'vue';
 import { Login } from '../models/login';
 import { useTokenStore } from '../stores/tokenStore';
 import { Constants } from 'src/stores/Constants';
+import { useVuelidate } from '@vuelidate/core';
+import { email, required } from '@vuelidate/validators';
+import { useQuasar } from 'quasar';
 
 const tokenStore = useTokenStore();
 
@@ -11,14 +14,33 @@ const login: Ref<Login> = ref({
   password: '',
 });
 
-async function onClick(e: any) {
+const rules = {
+  userName: { required, email },
+  password: { required },
+};
+
+const v$ = useVuelidate(rules, login);
+const $q = useQuasar();
+
+async function onSubmit(e: any) {
   e.preventDefault(); // mandatory; we choose when we navigate
   // then we never call go()
   try {
+    const isFormCorrect = await v$.value.$validate();
+    if (!isFormCorrect) {
+      for (var key in v$.value.$errors) {
+        const error = v$.value.$errors[key];
+        $q.notify({
+          message: `${error.$property}: ${error.$message}`,
+          color: 'red',
+        });
+      }
+    }
+    //console.log(`errors: ${v$.value.$validate()}`);
     await tokenStore.validateLogin(login.value);
     const token = tokenStore.Token;
-    if (!token) return;
-    console.log(`token: ${token.token}`);
+    // if (!token) return;
+    //console.log(`token: ${token.token}`);
     // Now store the token in the local storage
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     if (token) {
@@ -38,49 +60,53 @@ async function onClick(e: any) {
     <q-page-container>
       <q-page class="flex flex-center bg-grey-2">
         <q-card class="q-pa-md shadow-2 my_card" bordered>
-          <q-card-section class="text-center">
-            <div class="text-grey-9 text-h5 text-weight-bold">Sign in</div>
-            <div class="text-grey-8">Sign in below to access your account</div>
-          </q-card-section>
-          <q-card-section>
-            <q-input
-              dense
-              outlined
-              v-model="login.userName"
-              label="Email Address"
-            ></q-input>
-            <q-input
-              dense
-              outlined
-              class="q-mt-md"
-              v-model="login.password"
-              type="password"
-              label="Password"
-            ></q-input>
-          </q-card-section>
-          <q-card-section>
-            <q-btn
-              style="border-radius: 8px"
-              color="dark"
-              rounded
-              size="md"
-              label="Sign in"
-              no-caps
-              class="full-width"
-              @click="onClick"
-            ></q-btn>
-          </q-card-section>
-          <q-card-section class="text-center q-pt-none">
-            <div class="text-grey-8">
-              Don't have an account yet?
-              <a
-                href="#"
-                class="text-dark text-weight-bold"
-                style="text-decoration: none"
-                >Sign up.</a
-              >
-            </div>
-          </q-card-section>
+          <q-form @submit="onSubmit">
+            <q-card-section class="text-center">
+              <div class="text-grey-9 text-h5 text-weight-bold">Sign in</div>
+              <div class="text-grey-8">
+                Sign in below to access your account
+              </div>
+            </q-card-section>
+            <q-card-section>
+              <q-input
+                dense
+                outlined
+                v-model="login.userName"
+                label="Email Address"
+              ></q-input>
+              <q-input
+                dense
+                outlined
+                class="q-mt-md"
+                v-model="login.password"
+                type="password"
+                label="Password"
+              ></q-input>
+            </q-card-section>
+            <q-card-section>
+              <q-btn
+                style="border-radius: 8px"
+                color="dark"
+                rounded
+                size="md"
+                label="Sign in"
+                no-caps
+                class="full-width"
+                type="submit"
+              ></q-btn>
+            </q-card-section>
+            <q-card-section class="text-center q-pt-none">
+              <div class="text-grey-8">
+                Don't have an account yet?
+                <a
+                  href="#"
+                  class="text-dark text-weight-bold"
+                  style="text-decoration: none"
+                  >Sign up.</a
+                >
+              </div>
+            </q-card-section>
+          </q-form>
         </q-card>
       </q-page>
     </q-page-container>
