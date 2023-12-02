27ab8690359a2dfc1b00ce1d8a0eed2f84a8fac3
SK Dutta <skd@officeclip.com>
2023-12-01 20:27:08 -0500

    finetuned the profile data

--- src/models/profileInfo.ts
+++ src/models/profileInfo.ts
@@ -1,11 +1,11 @@
 export interface ProfileInfo {
   organizations: Organization[];
-  homeIcons: HomeIcon[];
+  applications: Application[];
   regionalSettings: RegionalSettings;
   profiles: Profile;
 }
 
-export interface HomeIcon {
+export interface Application {
   id: number;
   Name: string;
 }
@@ -37,47 +37,3 @@ export interface User {
 export interface RegionalSettings {
   shortDateFormat: string;
 }
-
-// JSON that is used to convert
-//{
-//    "organizations":[
-//        {
-//        "id": 1,
-//            "value": "Test Organization"
-//        }
-//    ],
-//    "homeIcons": [
-//        {
-//        "id": 1,
-//            "Name": "Calendar"
-//        }
-//    ],
-//    "regionalSettings": {
-//        "shortDateFormat": ""
-//    },
-//    "profiles":{
-//        "user":{
-//            "general": [
-//                {
-//                "name": "one",
-//                    "value": "two"
-//                }
-//                ],
-//             "timesheet": [
-//                {
-//                "name": "one",
-//                    "value": "two"
-//                }
-//            ]
-//        },
-//        "group": {
-//            "timesheet": [
-//                {
-//                "name": "one",
-//                    "value": "two"
-//                }
-//            ]
-//        }
-//    }
-
-//}
