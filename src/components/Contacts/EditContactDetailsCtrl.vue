0a2099fc9a02ef16af8d24ad344bbded554c3027
SK Dutta <skd@officeclip.com>
2024-01-03 16:11:02 -0500

    WIP

--- src/components/Contacts/EditContactDetailsCtrl.vue
+++ src/components/Contacts/EditContactDetailsCtrl.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable vue/no-mutating-props -->
 <script setup>
-import { defineProps, ref, onMounted, computed } from 'vue';
+import { defineProps, ref, onMounted, onBeforeMount, computed } from 'vue';
 import { useContactDetailsStore } from 'stores/ContactDetailsStore';
 
 const props = defineProps(['contactDetails']);
@@ -9,7 +9,7 @@ const dense = ref(false);
 
 const usecontactDetailsStore = useContactDetailsStore();
 
-usecontactDetailsStore.getContactLists();
+//usecontactDetailsStore.getContactLists();
 
 const getStates = computed(() => {
   return usecontactDetailsStore.states;
@@ -54,6 +54,16 @@ const updateCountry = (newValue) => {
   props.contactDetails.country_name = newValue.name;
 };
 
+const defaultCountry = computed(() => {
+  return getCountries.value.find((country) => country.is_default);
+});
+
+onBeforeMount(async () => {
+  await usecontactDetailsStore.getContactLists();
+  props.contactDetails.country_id = defaultCountry.value?.id;
+  props.contactDetails.country_name = defaultCountry.value?.name;
+});
+
 // onMounted(() => {
 //   //usecontactDetailsStore.getContactLists();
 //   if (props.contactDetails?.id == 0 && defaultState.value?.is_default == true) {
@@ -87,27 +97,86 @@ const updateCountry = (newValue) => {
   <div>
     <div class="q-pa-md">
       <div class="q-gutter-y-md column">
-        <q-input v-model="contactDetails.first_name" label="First Name *" placeholder="enter first name" :dense="dense"
-          lazy-rules :rules="[
-            (val) => (val && val.length > 0) || 'Please enter your first name',
-          ]"></q-input>
-        <q-input v-model="contactDetails.last_name" label="Last Name" placeholder="enter last name"
-          :dense="dense"></q-input>
-        <q-input v-model="contactDetails.title" label="Title" placeholder="enter title" :dense="dense"></q-input>
-        <q-input v-model="contactDetails.email" label="Email" placeholder="enter email address" :dense="dense"></q-input>
-        <q-select v-model="contactDetails.country_name" @update:model-value="updateCountry" option-value="id"
-          option-label="name" :options="getCountries" label="Country" placeholder="enter country name"
-          :dense="dense"></q-select>
-        <q-input v-model="contactDetails.work_phone" label="Work Phone" placeholder="work phone" :dense="dense"></q-input>
-        <q-input v-model="contactDetails.home_phone" label="Home Phone" placeholder="home phone" :dense="dense"></q-input>
-        <q-input v-model="contactDetails.street_address" label="Address" placeholder="street address"
-          :dense="dense"></q-input>
-        <q-input v-model="contactDetails.city" label="City" placeholder="City" :dense="dense"></q-input>
-        <q-select v-model="contactDetails.state_id" @update:model-value="updateState" option-value="id"
-          option-label="name" :options="getStates" label="State" placeholder="enter state name" emit-value map-options
-          :dense="dense" />
-        <q-input v-model="contactDetails.postal_code" label="Postal Code" placeholder="postal code"
-          :dense="dense"></q-input>
+        <q-input
+          v-model="contactDetails.first_name"
+          label="First Name *"
+          placeholder="enter first name"
+          :dense="dense"
+          lazy-rules
+          :rules="[
+            // (val) => (val && val.length > 0) || 'Please enter your first name',
+          ]"
+        ></q-input>
+        <q-input
+          v-model="contactDetails.last_name"
+          label="Last Name"
+          placeholder="enter last name"
+          :dense="dense"
+        ></q-input>
+        <q-input
+          v-model="contactDetails.title"
+          label="Title"
+          placeholder="enter title"
+          :dense="dense"
+        ></q-input>
+        <q-input
+          v-model="contactDetails.email"
+          label="Email"
+          placeholder="enter email address"
+          :dense="dense"
+        ></q-input>
+        <q-select
+          v-model="contactDetails.country_name"
+          @update:model-value="updateCountry"
+          option-value="id"
+          option-label="name"
+          :options="getCountries"
+          label="Country"
+          placeholder="enter country name"
+          :dense="dense"
+        ></q-select>
+        <q-input
+          v-model="contactDetails.work_phone"
+          label="Work Phone"
+          placeholder="work phone"
+          :dense="dense"
+        ></q-input>
+        <q-input
+          v-model="contactDetails.home_phone"
+          label="Home Phone"
+          placeholder="home phone"
+          :dense="dense"
+        ></q-input>
+        <q-input
+          v-model="contactDetails.street_address"
+          label="Address"
+          placeholder="street address"
+          :dense="dense"
+        ></q-input>
+        <q-input
+          v-model="contactDetails.city"
+          label="City"
+          placeholder="City"
+          :dense="dense"
+        ></q-input>
+        <q-select
+          v-model="contactDetails.state_id"
+          @update:model-value="updateState"
+          option-value="id"
+          option-label="name"
+          :options="getStates"
+          label="State"
+          placeholder="enter state name"
+          emit-value
+          map-options
+          :dense="dense"
+        />
+        <q-input
+          v-model="contactDetails.postal_code"
+          label="Postal Code"
+          placeholder="postal code"
+          :dense="dense"
+        ></q-input>
       </div>
     </div>
   </div>
