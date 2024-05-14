eb0f0d7de15f1c2ca2aeeb1cfe51cf683d665c76
Sudhakar Gundu <sudhakar@officeclip.com>
2024-05-14 12:48:45 +0530

    did the code refactor

--- src/stores/workFlow/WorkFlow.ts
+++ src/stores/workFlow/WorkFlow.ts
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia';
 import { workFlow, users } from '../../models/workFlow';
-import axios from 'axios';
+// import axios from 'axios';
 import { Constants } from 'stores/Constants';
 
 export const useWorkFlowStore = defineStore('workFlowStore', {
@@ -32,34 +32,6 @@ export const useWorkFlowStore = defineStore('workFlowStore', {
         console.error(error);
       }
     },
-
-    // async submitWorkFlow(workFlow: workFlow) {
-    //   try {
-    //     const instance = Constants.getAxiosInstance();
-    //    const response = await instance.put(
-    //       `${Constants.endPointUrl}/workflow/${workFlow.id}`,
-    //       workFlow
-    //     );
-    //     if (response.status === 200) {
-    //       //debugger;
-    //       this.workFlow = response.data;
-    //     }
-    //   } catch (error) {
-    //     console.error(`workFlow Error: ${error}`);
-    //   }
-    // },
-
-    // async submitWorkFlow(workFlow: workFlow) {
-    //   console.log('new workFlow Object', workFlow);
-
-    //   const callStr = `${Constants.endPointUrl}/workflow-summary`;
-    //   await fetch(callStr, {
-    //     method: 'POST',
-    //     body: JSON.stringify(workFlow),
-    //     headers: { 'Content-Type': 'application/json' },
-    //   });
-    //   console.log('new workFlow Object', this.workFlow);
-    // },
 
     async submitWorkFlow(workFlow: workFlow) {
       try {
