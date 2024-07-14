c5099f1ec15adcc61a9c93726ac99887d3c567ae
SK Dutta <skd@officeclip.com>
2024-07-13 20:19:35 -0400

    refactored

--- src/helpers/dateTimeHelper.ts
+++ src/helpers/dateTimeHelper.ts
@@ -15,11 +15,7 @@ const newExtractDateFromUtc = (
     return helper;
   } else {
     const date = new Date(utcDateTime);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Pad month with leading zero
-    const day = String(date.getDate()).padStart(2, '0'); // Pad day with leading zero
-    return `${year}-${month}-${day}`;
-    // return `${year}/${month}/${day}`;
+    return format(date, 'yyyy-MM-dd');
   }
 };
 
