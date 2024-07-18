275a9f3f05a6755aa8978ad004b995c77e8dd7aa
SK Dutta <skd@officeclip.com>
2024-07-18 14:29:56 -0400

    fixed the publicpath for build to /m/

--- quasar.config.js
+++ quasar.config.js
@@ -62,7 +62,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/mobile/',
+      publicPath: '/m/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
