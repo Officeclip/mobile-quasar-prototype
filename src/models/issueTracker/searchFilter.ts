41333d3a06023f1465cba00f8800ee4417dd3953
Sudhakar <sudhakar@officeclip.com>
2024-10-11 11:45:24 +0530

    Added filter model for issue tracker filters

---
+++ src/models/issueTracker/searchFilter.ts
@@ -0,0 +1,10 @@
+export interface searchFilter {
+  searchString: string;
+  status: number | string;
+  criticality: number | string;
+  category: number | string;
+  kind: number | string;
+  createdBy: string;
+  AssignedTo: string;
+  modifiedBy: string;
+}
