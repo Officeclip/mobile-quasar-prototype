28e267c0ff305f28c6b196ef8b3e9283863a3fec
SK Dutta <skd@officeclip.com>
2024-11-21 09:07:28 -0500

    test code

--- src/pages/TestMe.vue
+++ src/pages/TestMe.vue
@@ -1,101 +1,34 @@
-<script lang="ts" setup>
-import { ref } from 'vue';
-import CompA from '../../src/components/testme/CompA.vue';
-import dateTimeHelper from '../helpers/dateTimeHelper';
-
-const current = ref('test');
-const isValid = ref(false);
-
-const startDateTime = '2024-07-25T21:00:00Z';
-const isAllDayEvent = false;
-
-// const startDateTime = '2024-07-25';
-// const isAllDayEvent = true;
-
-const startDateModelValue = ref();
-startDateModelValue.value = dateTimeHelper.formatDateTimeFromRestAPIForUI(
-  startDateTime,
-  isAllDayEvent
-);
-
-const onSubmit = () => {
-  console.log();
-  const strDate = dateTimeHelper.formatDateTimeFromUIForRestAPI(
-    startDateModelValue.value,
-    isAllDayEvent
-  );
-  console.log(`onSubmit: ${strDate}`);
-};
-
-const dateTimeMask = 'ddd, MMM DD, YYYY hh:mm A';
-const dateMask = 'ddd, MMM DD, YYYY';
-const mask = (x: boolean) => {
-  return x ? dateMask : dateTimeMask;
-};
-
-const childComponent = ref(null);
-const onSave = () => {
-  alert(childComponent.value.validateAll());
-};
-</script>
-
 <template>
-  <div class="demo">
-    <q-form class="q-gutter-md" @submit="onSubmit">
-      <div>
-        <pre>startDateModelValue = {{ startDateModelValue }}</pre>
-        <q-input v-model="startDateModelValue" label="Starts*" readonly>
-          <template v-slot:prepend>
-            <q-icon class="cursor-pointer" name="event">
-              <q-popup-proxy
-                cover
-                transition-hide="scale"
-                transition-show="scale"
-              >
-                <q-date
-                  v-model="startDateModelValue"
-                  :mask="mask(isAllDayEvent)"
-                  ><div class="row items-center justify-end">
-                    <q-btn v-close-popup color="primary" flat label="Close" />
-                  </div>
-                </q-date>
-              </q-popup-proxy>
-            </q-icon>
-          </template>
-
-          <template v-if="!isAllDayEvent" v-slot:append>
-            <q-icon class="cursor-pointer" name="access_time">
-              <q-popup-proxy
-                cover
-                transition-hide="scale"
-                transition-show="scale"
-              >
-                <q-time
-                  v-model="startDateModelValue"
-                  :mask="mask(isAllDayEvent)"
-                >
-                  <div class="row items-center justify-end">
-                    <q-btn v-close-popup color="primary" flat label="Close" />
-                  </div>
-                </q-time>
-              </q-popup-proxy>
-            </q-icon>
-          </template>
-        </q-input>
-
-        <CompA :current="current" ref="childComponent" />
-      </div>
-    </q-form>
-    <div>
-      <q-btn
-        class="q-ml-sm"
-        color="primary"
-        flat
-        label="Submit"
-        no-caps
-        type="submit"
-      />
-      <q-btn id="external-button" @click="onSave">External Button</q-btn>
+  <div class="q-pa-md">
+    <q-input v-model="url" label="Enter URL" type="text" />
+    <q-btn label="Check URL" @click="checkUrl" color="primary" />
+    <div v-if="showMessage" class="q-mt-sm">
+      <q-alert v-if="isValid" type="positive" :value="true">
+        Valid URL!
+      </q-alert>
+      <q-alert v-else type="negative" :value="true">
+        Invalid URL. Please check the URL.
+      </q-alert>
     </div>
   </div>
 </template>
+
+<script>
+import { ref, computed } from 'vue';
+
+export default {
+  setup() {
+    const url = ref('');
+    const urlRegex =
+      /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+    const isValid = computed(() => urlRegex.test(url.value));
+    const showMessage = ref(false);
+
+    const checkUrl = () => {
+      showMessage.value = true;
+    };
+
+    return { url, isValid, checkUrl, showMessage };
+  },
+};
+</script>
