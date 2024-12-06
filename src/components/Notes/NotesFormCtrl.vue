32941b3860052f1c476123476b4ffe93bf04b8fa
Nagesh Kulkarni <nagesh@officeclip.com>
2024-12-06 18:11:11 +0530

    In Notes > Edit all icons of the notes description are not there - Fixed.

--- src/components/Notes/NotesFormCtrl.vue
+++ src/components/Notes/NotesFormCtrl.vue
@@ -61,6 +61,50 @@ defineExpose({
               v-model="notesDetail.description"
               paragraph-tag="div"
               placeholder="enter description"
+              :toolbar="[
+                [
+                  {
+                    label: $q.lang.editor.formatting,
+                    icon: $q.iconSet.editor.formatting,
+                    list: 'no-icons',
+                    options: ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'code'],
+                  },
+                  {
+                    label: $q.lang.editor.fontSize,
+                    icon: $q.iconSet.editor.fontSize,
+                    fixedLabel: true,
+                    fixedIcon: true,
+                    list: 'no-icons',
+                    options: [
+                      'size-1',
+                      'size-2',
+                      'size-3',
+                      'size-4',
+                      'size-5',
+                      'size-6',
+                      'size-7',
+                    ],
+                  },
+                  'removeFormat',
+                ],
+                [
+                  'link',
+                  'bold',
+                  'italic',
+                  'fullscreen',
+                  'unordered',
+                  'ordered',
+                ],
+                [
+                  {
+                    label: $q.lang.editor.align,
+                    icon: $q.iconSet.editor.align,
+                    fixedLabel: true,
+                    list: 'only-icons',
+                    options: ['left', 'center', 'right', 'justify'],
+                  },
+                ],
+              ]"
             ></q-editor>
           </template>
         </q-field>
