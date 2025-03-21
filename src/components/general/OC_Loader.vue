754714b1cc97d92e0c093d5e03167cf944084b10
Sudhakar <sudhakar@officeclip.com>
2025-03-21 16:30:09 +0530

    fixed the position of the q-spinner

--- src/components/general/OC_Loader.vue
+++ src/components/general/OC_Loader.vue
@@ -47,9 +47,12 @@ defineProps({
 .loader-container {
   height: 100%;
   width: 100%;
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: rgba(255, 255, 255, 0.8);
   z-index: 9999;
 }
