375ed7fa9b941467c8cd3b9930553a4ed0233840
Sudhakar Gundu <sudhakar@officeclip.com>
2025-08-11 17:23:24 +0530

    did new android app version 0.7.1

--- src-capacitor/android/app/src/main/java/org/capacitor/quasar/officeclipsuite/MainActivity.java
+++ src-capacitor/android/app/src/main/java/org/capacitor/quasar/officeclipsuite/MainActivity.java
@@ -1,5 +1,31 @@
 package org.capacitor.quasar.officeclipsuite;
 
+import android.os.Bundle;
+import android.os.Build;
+import android.view.View;
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Enable edge-to-edge mode for the activity
+        EdgeToEdge.enable(this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+          WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView())
+          .setAppearanceLightStatusBars(true);
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content), (view, insets) -> {
+            Insets systemBarsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout());
+            view.setPadding(systemBarsInsets.left, systemBarsInsets.top, systemBarsInsets.right, systemBarsInsets.bottom);
+            return insets;
+        });
+        }
+}
